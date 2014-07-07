ff09c44edf466ae669560e4fb3ee52d86c360cd3
Fernando Ferreira <bse.fernando@gmail.com>
2014-07-07 18:15:14 -0300

    Karatsuba's algorithm

--- Algorithms/Karatsuba/karatsuba.c
+++ Algorithms/Karatsuba/karatsuba.c
@@ -2,27 +2,25 @@
 
 typedef long long LL;
 
-LL karatsuba(x, y, n) {
-    if(n == 1) return x * y;
+LL karatsuba(LL x, LL y, LL n) {
+    LL x0, x1, y0, y1, z0, z1, z2;
 
-    LL m, i, k = n / 2;
-    LL x0, x1, y0, y1;
-    LL z0, z1, z2;
+    x1 = x >> 16;
+    y1 = y >> 16;
+    x0 = x & 0xFFFF;
+    y0 = y & 0xFFFF;
 
-    for(m = i = 0; i < k; i++) m = (m << 1) + 1;
+    z2 = x1 * y1;
+    z0 = x0 * y0;
+    z1 = ( x0 + x1 ) * ( y0 + y1 ) - z0 - z2;
 
-    x0 = x & m; x1 = x >> k;
-    y0 = y & m; y1 = y >> k;
-
-    z0 = karatsuba(x0, y0, k);
-    z2 = karatsuba(x1, y1, k);
-    z1 = karatsuba(x0 + x1, y0 + y1, k) - z0 - z2;
-
-    return (z2 << n) + (z1 << k) + z0;
+    return ( z2 << 32 ) + ( z1 << 16 ) + z0;
 }
 
 int main() {
-    printf("%lld\n", karatsuba(1234, 5678, 1));
+    int i;
+    LL x = 0;
 
+    printf("%lld\n", karatsuba(1234, 5678, 1));
     return 0;
 }
